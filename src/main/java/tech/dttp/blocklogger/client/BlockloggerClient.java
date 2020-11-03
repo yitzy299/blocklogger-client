e3e4c386253c75a3ebff71ea3a0507de8d59bddd
yitzy299 <hugocarter@hotmail.co.uk>
2020-11-03 21:22:55 +0000

    Added mod framework
    The normal for a new mod

---
+++ src/main/java/tech/dttp/blocklogger/client/BlockloggerClient.java
@@ -0,0 +1,10 @@
+package tech.dttp.blocklogger.client;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlockloggerClient implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
